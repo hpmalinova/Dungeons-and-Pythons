2ea8c1d508587936789da59f27169945529dc348
Hristiyana <hpmalinova@github.com>
2020-04-01 20:11:34 +0300

    Implement spell  class

---
+++ spell.py
@@ -0,0 +1,12 @@
+class Spell:
+	def __init__(self, name, damage, mana_cost, cast_range):
+		self.name = name
+		
+		assert damage >= 0, 'Damage dealt can`t be a negative number.'
+		self.damage = name
+
+		assert mana_cost >= 0, 'Mana cost can`t be a negative number.'
+		self.mana_cost = mana_cost
+
+		assert cast_range >= 0, 'Cast range can`t be a negative number.'
+		self.cast_range = cast_range
